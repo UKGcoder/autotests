374d0a7ce8fc72fa109895246feea04c9e028321
shenderrex <r.aminov@g.nsu.ru>
2023-04-14 17:10:05 +0700

    first testing jenkins

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import undetected_chromedriver as uc
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+options = webdriver.ChromeOptions()
+driver = uc.Chrome(options=options)
+driver.get(url="https://develop.icrm.liss.pro/#login")
+time.sleep(3)
+driver.find_element(By.XPATH, "//*[@id=\"login_email\"]").clear()
+driver.find_element(By.XPATH, "//*[@id=\"login_email\"]").send_keys("testing@selenium.com")
+driver.find_element(By.XPATH, "//*[@id=\"login_password\"]").clear()
+driver.find_element(By.XPATH, "//*[@id=\"login_password\"]").send_keys("aksjhd2912t7sai7w")
+time.sleep(3)
+driver.find_element(By.XPATH, "//*[@id=\"page-login\"]/div/main/div[2]/div/section[1]/div/form/div[2]/button").click()
+time.sleep(3)
+assert "app" in driver.current_url
