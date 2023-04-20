8e4b50a3f4e44097370afb9a645e2194fe5bdc84
Shabalin Artyom <artem2010_01@mail.ru>
2023-04-20 22:12:51 +0700

    fix test #11

--- script11.py
+++ script11.py
@@ -57,8 +57,8 @@ def test_new_deal_creator(test_setup):
         driver.find_element(By.XPATH, "/html/body/div[5]/div/div/div[1]/div[2]/button[2]").click()
     except:
         pass
-    getClick('//*[@id="page-Workspaces"]/div[2]/div[2]/div/div[3]/div[1]/div[1]/div/div[1]/a[4]')
-    #driver.get("https://develop.icrm.liss.pro/app/%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0-%D1%81%D0%BE-%D1%81%D0%B4%D0%B5%D0%BB%D0%BA%D0%B0%D0%BC%D0%B8")
+    #getClick('//*[@id="page-Workspaces"]/div[2]/div[2]/div/div[3]/div[1]/div[1]/div/div[1]/a[4]')
+    driver.get("https://develop.icrm.liss.pro/app/%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0-%D1%81%D0%BE-%D1%81%D0%B4%D0%B5%D0%BB%D0%BA%D0%B0%D0%BC%D0%B8")
     try:
         assert WebDriverWait(driver, 10).until(EC.url_contains(
             "https://develop.icrm.liss.pro/app/%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0-%D1%81%D0%BE-%D1%81%D0%B4%D0%B5%D0%BB%D0%BA%D0%B0%D0%BC%D0%B8"))
