cfe9de774d5bed53c83002d11d417cb4950dcfd2
Shabalin Artyom <artem2010_01@mail.ru>
2023-05-12 21:29:31 +0700

    add check for input and save in deal fields

--- deal_changer.py
+++ deal_changer.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 
 def inputField(xpath, driver: uc.Chrome, text):
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
     field = driver.find_element(By.XPATH, xpath)
     field.clear()
     field.send_keys(text)
